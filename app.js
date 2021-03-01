848b51dc9ea258b1b8360bfa4646f75b282410f9
Paul Parant <paul.parant@gmail.com>
2021-03-01 23:44:17 +0100

    tout naze

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
+const beers = punkAPI.getBeers();
+
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
@@ -20,6 +22,21 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res) => {
+  res.render('beers');
+
+  punkAPI
+  .getBeers()
+  .then(beersFromApi => console.log('Beers from the database: ', beersFromApi))
+  .catch(error => console.log(error));
+
+});
+
+
+app.get('/random-beers', (req, res) => {
+  res.render('random-beers');
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
